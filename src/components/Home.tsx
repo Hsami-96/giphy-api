f633619a68a1ffd0d48fe0ce89af77efcb1e0ef3
Hani <h.samir@edu.salford.ac.uk>
2022-12-11 01:16:41 +0000

    displaying trends data using offset on the api call

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,21 @@
 import { Grid } from "@giphy/react-components";
 import { useFetchAPI } from "../hooks/use-fetch-api";
-
+import './Home.css'
 export const Home = () => {
   const {
-    data: trendsData,
-    loading: trendsLoading,
+    getTrendingData: getTrendsData,
     error: trendsError,
   } = useFetchAPI({ url: "https://api.giphy.com/v1/gifs/trending" });
 
-  console.log(trendsData)
+  const fetchGifs = (offset: number) => getTrendsData(offset)
+
   return (
-    <div>
-      {/* <Grid 
-        initialGifs={trendsData}
-      /> */}
+    <div className="gifsContainer">
+        <Grid
+          width={800}
+          columns={3}
+          fetchGifs={fetchGifs}
+        />
     </div>
   )
 }
--- src/hooks/use-fetch-api.ts
+++ src/hooks/use-fetch-api.ts
@@ -1,51 +1,31 @@
+import { GifsResult } from "@giphy/js-fetch-api";
 import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { IGif } from '@giphy/js-types';
-import { GifProperties } from "../models/GifProperties";
-import { mapToGifData } from "../utils/MapToGifData";
+import { useState } from "react";
 
 interface useFetchAPIProps {
-  url: string;
+  url: string,
 }
 const apiKey: string = process.env.REACT_APP_GIPHY_KEY ?? ''
 
 export const useFetchAPI = ({ url }: useFetchAPIProps) => {
-  const [data, setData] = useState<GifProperties[]>([]);
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    if (url) {
-      setLoading(true);
-      const getData = async () => {
-        await axios(url, {
-          params: {
-            api_key: apiKey,
-          }
+      const getTrendingData = async (offset: number): Promise<GifsResult> => {
+        return axios(url, {
+            params: {
+                api_key: apiKey,
+                offset: offset,
+                limit: 10
+            }
+        }).then((data) => {
+            return data.data 
+        }).catch((error) => {
+            setError(error);
         })
-        .then((response) => {
-          if(response.data.data) {
-            let gifProperties: GifProperties[] = []
-            response.data.data.map((gif: IGif) => {
-                const mapToGifProperty = mapToGifData(gif)
-                gifProperties.push(mapToGifProperty)
-            })
-
-            setData(gifProperties);          
-          }
-        })
-        .catch((error) => {
-          setError(error);
-        })
-        .finally(() => setLoading(false));
       }
-      getData()
-    }
-  }, [url]);
 
   return {
-    data,
-    loading,
+    getTrendingData,
     error
   }
 };
